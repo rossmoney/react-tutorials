20562d97692248beb06bfe895c5ce209c5c6b864
Ross Money <5647850+rossmoney@users.noreply.github.com>
2025-03-22 00:16:02 +0000

    dynamic list

--- src/LinkList.tsx
+++ src/LinkList.tsx
@@ -10,9 +10,32 @@ export default function LinkList(props: { links: LinksSimple | LinksCategories }
   return (
     <>
       {props.links.map((linkSet) => {
-        let entries = ('href' in linkSet) ? [linkSet] : linkSet.entries;
+        let entries = 'href' in linkSet ? [linkSet] : linkSet.entries;
+        let list = 'href' in linkSet ? (
+          <ListItem key={linkSet.href} disablePadding>
+            <ListItemButton component="a" href={linkSet.href}>
+              <ListItemIcon>{linkSet.icon}</ListItemIcon>
+              <ListItemText primary={linkSet.text} />
+            </ListItemButton>
+          </ListItem>
+        ) : (
+          <List>
+            {entries.map(({ href, text, icon }) => (
+              <ListItem key={href} disablePadding>
+                <ListItemButton component="a" href={href}>
+                  <ListItemIcon>{icon}</ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        );
+
+        function getRandomInt(max: number) {
+          return Math.floor(Math.random() * max);
+        }
         return (
-          <React.Fragment key={'category' in linkSet ? linkSet.category : new Date().getTime()}>
+          <React.Fragment key={'category' in linkSet ? linkSet.category : new Date().getTime() + getRandomInt(1000)}>
             {'category' in linkSet && (
               <>
                 <Typography variant="h6" component="h2" gutterBottom>
@@ -21,16 +44,7 @@ export default function LinkList(props: { links: LinksSimple | LinksCategories }
                 <Divider />
               </>
             )}
-            <List>
-              {entries.map(({ href, text, icon }) => (
-                <ListItem key={href} disablePadding>
-                  <ListItemButton component="a" href={href}>
-                    <ListItemIcon>{icon}</ListItemIcon>
-                    <ListItemText primary={text} />
-                  </ListItemButton>
-                </ListItem>
-              ))}
-            </List>
+            {list}
           </React.Fragment>
         );
       })}
